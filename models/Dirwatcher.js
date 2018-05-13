ca10fe8a8836646d4f8ce0c79c78ebbe5bdb4f23
JulsD <dutchak.julia@gmail.com>
2018-05-13 14:51:05 +0200

    HW2: add diff variables to dirWatcher Constructor

--- models/Dirwatcher.js
+++ models/Dirwatcher.js
@@ -4,26 +4,20 @@ import { readdir } from 'fs';
 
 
 class DirWatcher extends EventEmitter {
-    constructor() {
+    constructor(filesList = [], addedFiles = [], deletedFiles = []) {
         super();
-        this.filesList = [];
-    }
-
-    isNewFilesAdded(diff) {
-        return diff && diff.addedFiles && diff.addedFiles.length > 0
+        this.filesList = filesList;
+        this.addedFiles = addedFiles;
+        this.deletedFiles = deletedFiles;
     }
 
     findDiff(files, dirPath) {
-        let addedFiles, deletedFiles;
-
         if (!this.filesList.length) {
-            addedFiles = files;
+            this.addedFiles = files;
         } else {
-            addedFiles = files.filter(x => !this.filesList.includes(x));
-            deletedFiles = this.filesList.filter(x => !files.includes(x));
+            this.addedFiles = files.filter(x => !this.filesList.includes(x));
+            this.deletedFiles = this.filesList.filter(x => !files.includes(x));
         }
-
-        return {addedFiles, deletedFiles};
     }
 
     watch(dirPath, delay) {
@@ -31,9 +25,9 @@ class DirWatcher extends EventEmitter {
             readdir(dirPath, (err, files) => {
                 if(err) throw err;
                 if (files && files.length > 0) {
-                    let diff = this.findDiff(files, dirPath);
-                    if (this.isNewFilesAdded(diff)) {
-                        diff.addedFiles
+                    this.findDiff(files, dirPath);
+                    if (this.addedFiles.length > 0) {
+                        this.addedFiles
                             .map((fileName) => path.join(dirPath, fileName))
                             .filter(e => e.split('.').pop() === 'csv')
                             .forEach(filePath => this.emit('changed', filePath));
