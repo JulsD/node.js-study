fb007c21dc09adefd261e55b7c3642d37326f371
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2018-05-22 19:58:03 +0200

    HW3: add list of actions

--- utils/streams.js
+++ utils/streams.js
@@ -10,25 +10,63 @@ program
 
 if (!process.argv.slice(2).length) {
     program.outputHelp(addWarning);
+} 
+else if (program.action == 'reverse') {reverse();} 
+else if (program.action == 'transform') {transform();}
+else if (program.action == 'filePath') {filePath();} 
+else if (program.action == 'outputFile') {
+    if(program.file) {
+        outputFile(program.file);
+    } else {
+        console.log('File path should be spesified');
+    }
 }
+else if (program.action == 'convertFromFile') {
+    if(program.file) {
+        convertFromFile(program.file);
+    } else {
+        console.log('File path should be spesified');
+    }
+}
+else if (program.action == 'convertToFile') {
+    if(program.file) {
+        convertToFile(program.file);
+    } else {
+        console.log('File path should be spesified');
+    }
+} else {
+    console.log('Action doesn\'t exist');
+}
+
   
 function addWarning(txt) {
     let warning = colors.red('\n  Wrong input: module was called without arguments. \n');
     return warning + txt;
 } 
 
+
+// list of actions:
 function reverse(str) { 
-    /* ... */
+    // process.stdin.pipe(process.stdout);
+    // let stdin = process.stdin, stdout = process.stdout;
+
+    // stdin.resume();
+    // stdin.setEncoding('utf8');
+
+    // stdin.on('data', function(str) {
+    //     stdout.write(str);
+    // });
+    console.log('reverse');
 }
 function transform(str) { 
-    /* ... */
+    console.log('transform');
 }
 function outputFile(filePath) { 
-    /* ... */
+    console.log('outputFile:', filePath);
 }
 function convertFromFile(filePath) { 
-    /* ... */
+    console.log('convertFromFile:', filePath);
 }
 function convertToFile(filePath) { 
-    /* ... */
+    console.log('convertToFile:', filePath);
 }
