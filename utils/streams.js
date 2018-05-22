40f54e84b36aaa4854c13c92e09ea64fa1a037b9
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2018-05-22 19:30:30 +0200

    HW3: update help output if there were no args

--- utils/streams.js
+++ utils/streams.js
@@ -1,12 +1,21 @@
 const program = require('commander');
+var colors = require('colors');
 
 program
-  .option('-a, --action', 'Runs some action')
-  .parse(process.argv);
+    .version('0.0.1')
+    .usage('[options] <keywords>')
+    .option('-a, --action <type>', 'Runs action of given type')
+    .option('-f, --file <filePath>', 'Opens a file by given path')
+    .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-    program.outputHelp();
+    program.outputHelp(addWarning);
 }
+  
+function addWarning(txt) {
+    let warning = colors.red('\n  Wrong input: module was called without arguments. \n');
+    return warning + txt;
+} 
 
 function reverse(str) { 
     /* ... */
