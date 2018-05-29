aa95c218b1dd1a66a60dea8e65c5281bbab72fe8
JulsD <dutchak.julia@gmail.com>
2018-05-30 00:56:17 +0200

    HW4: add index.html file with message var  to html-server

--- http-servers/html-server.js
+++ http-servers/html-server.js
@@ -1,6 +1,34 @@
-var http = require('http');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+function templateEngine(template, data) {
+    let vars = template.match(/\{\w+\}/g);
+
+    if (!vars) {
+      return template;
+    }
+
+    let nonVars = template.split(/\{\w+\}/g);
+    let output = '';
+
+    for (let i = 0; i < nonVars.length; i++) {
+      output += nonVars[i];
+
+      if (i < vars.length) {
+        let key = vars[i].replace(/[\{\}]/g, '');
+        output += data[key]
+      }
+    }
+
+    return output;
+  };
 
 http.createServer(function (req, res) {
-    res.write('Hello World!'); 
-    res.end(); 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    let template = fs.readFileSync(path.resolve(__dirname, 'index.html'));
+
+    res.write(templateEngine(template.toString(), {message: 'Hello World! My message is HERE!'}));
+    res.end();
 }).listen(8080); 
