2a45591967bde58db38f6616949c7920e6a21947
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2018-06-05 20:38:10 +0200

    HW4: add json text to json-server

--- http-servers/json-server.js
+++ http-servers/json-server.js
@@ -1,6 +1,22 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.write('Hello World!'); 
-  res.end(); 
-}).listen(8080); 
+const product = {
+  id: 1,
+  name: 'Supreme T-Shirt',
+  brand: 'Supreme',
+  price: 99.99,
+  options: [
+      { color: 'blue' },
+      { size: 'XL' }
+  ]
+};
+
+http.createServer()
+    .on('request', function (req, res) {
+      let { method } = req;
+        if(method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(product)); 
+        res.end();
+      }
+    }).listen(8080); 
