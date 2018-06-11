af2531fbb8befc245a92c18119ac3b90e664d94e
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2018-06-11 17:09:15 +0200

    HW4: update Product model

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,35 @@
+import { find } from 'lodash';
+
 class Product {
-    constructor() {
-        console.log("Product module");
+    constructor(products) {
+        this.products = products? products : [];
+    }
+
+    add(product) {
+        let newId = this.createId();
+        product.id = newId;
+        return this.products.push(product);
+    }
+
+    get(productID) {
+        let product = find(this.products, function(o){ return o.id == productID });
+        if(product) {
+            return product;
+        } else {
+            return {"product": "doesn't exist"}
+        }
+    }
+
+    fetchAll() {
+        return this.products;
+    }
+
+    createId() {
+        let newId = Math.random()*10;
+        if(find(this.products, { id: newId})) {
+            this.createId();
+        }
+        return newId;
     }
 }
 
