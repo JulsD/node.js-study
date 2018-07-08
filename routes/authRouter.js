2577b82f925a193d27ff08bc621f73a3996e32fb
JulsD <dutchak.julia@gmail.com>
2018-07-08 20:01:07 +0200

    HW5: add generation of token

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,23 +1,46 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import { find } from 'lodash';
 const authRouter = express.Router();
 
 const loginBase = [
     {
         login: 'admin',
-        password: 'admin password'
+        password: 'admin password',
+        email: "admin@ad.min",
+        username: "Admin"
     }, {
         login: 'test',
-        password: 'test password'
+        password: 'test password',
+        email: "test@te.st",
+        username: "Test"
     }
 ]
 
 authRouter.route('/auth').post(function (req, res) {
-    const isInBase = find(loginBase, {login: req.body.login, password: req.body.password});
-    if(!isInBase) {
-            res.status(403).json("There is no such user");
+    const user = find(loginBase, {login: req.body.login, password: req.body.password});
+    if(!user) {
+        const isLoginInBase = find(loginBase, {login: req.body.login});
+        const isPasswordInBase = find(loginBase, {password: req.body.password});
+        let errorData = "";
+        if (!isLoginInBase) {
+            errorData = "The login name is not right";
+        } else if (!isPasswordInBase) {
+            errorData = "Pasword name is not right";
+        } else {
+            errorData = "There is no such user";
+        }
+
+        res.status(404).send({"message": "Not Found", "data": errorData});
     } else {
-        next();
+        let data = {
+            "user": {
+                "email": user.email,
+                "username": user.username
+            }
+        }
+        let token = jwt.sign(data, 'someSecret', {expiresIn: 10});
+        res.status(200).send({ "message": "OK", data, "token": token});
     }
 });
 
